9f4dbb2169bb4b1227a1828d68a84b465884cb8d
Yuki Matsuda <y.mazun@gmail.com>
2015-12-21 01:06:59 +0900

    Add sources

---
+++ Assets/ChinoMessage.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class ChinoMessage : MonoBehaviour {
+	public int messageChangeInterval = 5;
+	[SerializeField] Text message = null;
+
+	private string[] randomMessages = {
+		"いらっしゃいませ",
+		"ありがとうございます",
+		"よろしければ見ていってください"
+	};
+
+	private DateTime nextMessageChange;
+
+	// Use this for initialization
+	void Start () {
+		ChangeMessage (randomMessages [0]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (nextMessageChange.CompareTo (DateTime.Now) < 0) {
+			ChangeMessage (randomMessages [UnityEngine.Random.Range(0, randomMessages.Length)]);
+		}
+	}
+
+	public void MessageForBook() {
+		ChangeMessage("シャロさんの同人誌です。\n500円になります。");
+	}
+
+	public void MessageForBadge() {
+		ChangeMessage("私達の缶バッジです。\n200円になります。");
+	}
+
+	private void ChangeMessage(String text) {
+		message.text = text;
+		nextMessageChange = DateTime.Now.AddSeconds (messageChangeInterval);
+	}
+}
