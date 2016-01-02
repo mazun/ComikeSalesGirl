d0fd6b3b79e82737af935c9a82b77d206b53049c
y_mazun <y.mazun@gmail.com>
2016-01-02 19:13:27 +0900

    C89完成版

---
+++ Assets/Scripts/AutoScale.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoScale : MonoBehaviour {
+	[SerializeField] float min = 1.0f;
+	[SerializeField] float max = 1.3f;
+
+	[SerializeField] float period = 2f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float scale = min + (max - min) * (1 + Mathf.Sin (Time.time / period * 2 * Mathf.PI)) / 2;
+		transform.localScale = new Vector3 (scale, scale);
+	}
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# README
+
+## 動かし方
+
+Unityはインストール済みの前提とする。  
+ライセンスの都合からSDKおよびフォントは含めていないので、以下の手順で実行する。
+
+1. http://sites.cybernoids.jp/cubism-sdk2/unity2-1 からSDKをダウンロードして展開する。展開した中身のうちのsample/Demo/ に本リポジトリないのファイルを全てコピーする。
+2. http://azukifont.com/font/uzura.html からうずらフォントをダウンロードして展開する。uzura.ttfをsample/Demo/Assets/uzura.ttfにコピーする。
+3. sample/Demo/Assets/Scene/main.unity を開く。
